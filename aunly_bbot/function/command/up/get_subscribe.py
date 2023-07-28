109779c2dcdedd3688ebc48f7d11132c53de6a19
Well404 <well_404@outlook.com>
2023-07-28 21:34:55 +0800

    :construction: 添加是否在直播中的提示

--- aunly_bbot/function/command/up/get_subscribe.py
+++ aunly_bbot/function/command/up/get_subscribe.py
@@ -1,20 +1,22 @@
-from graia.saya import Channel
 from graia.ariadne.app import Ariadne
-from graia.ariadne.model import Group
-from graia.ariadne.message.element import At
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.saya.builtins.broadcast.schema import ListenerSchema
+from graia.ariadne.message.element import At
 from graia.ariadne.message.parser.twilight import (
-    Twilight,
-    RegexMatch,
     ElementMatch,
     ElementResult,
+    RegexMatch,
+    Twilight,
 )
-
+from graia.ariadne.model import Group
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+import time
+from ....core import BOT_Status
 from ....core.bot_config import BotConfig
-from ....core.data import get_sub_by_group
 from ....core.control import Interval, Permission
+from ....core.data import get_sub_by_group
+from ....utils.time_tools import calc_time_total
 
 channel = Channel.current()
 
@@ -34,14 +36,15 @@ async def sub_list(app: Ariadne, group: Group, at: ElementResult):
         if at_element.target != BotConfig.Mirai.account:
             return
     sublist = get_sub_by_group(group.id)
+
     sublist_count = len(sublist)
     if sublist_count == 0:
         await app.send_group_message(group, MessageChain("本群未订阅任何 UP"))
     else:
         msg = [f"本群共订阅 {sublist_count} 个 UP\n注：带*号的表示该 UP 已被设定自定义昵称"]
-        msg.extend(
-            f"\n{i}. {f'*{data.nick}' if data.nick else data.uname}（{data.uid}）"
-            for i, data in enumerate(sublist, 1)
-        )
+        for i, sub in enumerate(sublist, 1):
+            if sub.uid in BOT_Status.living:
+                live = f" - 正在直播: {calc_time_total(time.time() - BOT_Status.living[sub.uid])}"
+            msg.append(f"\n{i}. {f'*{sub.nick}' if sub.nick else sub.uname}（{sub.uid}）{live}")
 
         await app.send_group_message(group, MessageChain(msg))
